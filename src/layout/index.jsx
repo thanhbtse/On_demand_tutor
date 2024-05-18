c20368d88299a7ccc834c51b9153d397f75d0bf4
ThanhB <bintri92@gmail.com>
2024-05-18 09:14:46 +0700

    update layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -33,11 +33,7 @@ const App = ({ children }) => {
           style={{ display: "flex", alignItems: "center", marginLeft: "100px" }}
         >
           <div>
-            <img
-              width={60}
-              height={60}
-              src={hatlogo}
-            />
+            <img width={60} height={60} src={hatlogo} />
           </div>
           <div className="flex flex-col mt-1 ml-2 responsive-hide">
             <span className="text-white leading-tight flex flex-col">
@@ -57,7 +53,7 @@ const App = ({ children }) => {
           <Input
             placeholder="Nhập từ khóa để tìm các lớp dạy kèm"
             onSearch={(value) => console.log(value)}
-            className="w-[500px] h-10 rounded-none"
+            className="w-full md:w-[500px] h-10 rounded-none"
             enterButton
           />
           <div className="space-x-2">
@@ -70,149 +66,160 @@ const App = ({ children }) => {
           </div>
         </div>
       </Header>
-      <Content
-        style={{
-          padding: "0 200px",
-        }}
+      <div
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
-        <div
+        <div style={{ flex: "1 0 auto" }}>
+          <Content
+            style={{
+              padding: "0 200px",
+            }}
+          >
+            <div
+              style={{
+                padding: 24,
+                minHeight: 380,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              {children}
+            </div>
+          </Content>
+        </div>
+        <Footer className=" bottom-20">
+          <div className="ml-3 md:ml-12 mr-2 md:mr-10">
+            <Row gutter={[16, 16]} className="md:gutter-[48,16]">
+              <Col className="m-auto md:m-0">
+                <div className="flex flex-col space-y-2 md:space-y-6 font-mono">
+                  <span className="font-bold text-[18px]">GIASUONLINE.VN</span>
+                  <span className="font-normal text-[14px]">
+                    Địa chỉ: Lưu Hữu Phước, Đông Hoà, Dĩ An, Bình Dương
+                  </span>
+                  <span className="font-normal text-[14px]">
+                    Điện thoại: 0989897338
+                  </span>
+                  <span className="font-normal text-[14px]">
+                    Email: contact@giasuonline.vn
+                  </span>
+                </div>
+              </Col>
+              <Col className="ml-0 md:ml-[200px]">
+                <div className="flex flex-col space-y-2 md:space-y-6 font-mono">
+                  <span className="font-bold text-[18px]">LIÊN KẾT</span>
+                  <div className="flex flex-row divide-x-2 divide-solid space-x-2 text-[#ff4778]">
+                    <Link className="font-normal text-[14px]" to="/gioi-thieu">
+                      Giới Thiệu
+                    </Link>
+                    <Link
+                      className="font-normal pl-2 text-[14px]"
+                      to="/lien-he"
+                    >
+                      Liên Hệ
+                    </Link>
+                  </div>
+                  <div className="flex flex-row divide-x-2 divide-solid space-x-2 text-[#ff4778]">
+                    <Link className="font-normal text-[14px]" to="/tai-lieu">
+                      Tài Liệu
+                    </Link>
+                    <Link
+                      className="font-normal pl-2 text-[14px]"
+                      to="/kien-thuc"
+                    >
+                      Kiến Thức
+                    </Link>
+                    <Link
+                      className="font-normal pl-2 text-[14px]"
+                      to="/kinh-nghiem"
+                    >
+                      Kinh Nghiệm
+                    </Link>
+                  </div>
+                  <Link className="text-[#ff4778] text-[14px]" to="/login">
+                    Tài Khoản
+                  </Link>
+                </div>
+              </Col>
+              <Col className="ml-0 md:ml-[150px]">
+                <div className="flex flex-col space-y-2 md:space-y-6 font-mono">
+                  <span className="font-bold text-[18px]">CHÍNH SÁCH</span>
+                  <Link
+                    className="text-[#ff4778] text-[14px]"
+                    to="/chinh-sach-nguoi-dung"
+                  >
+                    Chính Sách Người Dùng
+                  </Link>
+                  <Link
+                    className="text-[#ff4778] text-[14px]"
+                    to="/dieu-khoan-su-dung"
+                  >
+                    Điều Khoản Sử Dụng
+                  </Link>
+                  <Link
+                    className="text-[#ff4778] text-[14px]"
+                    to="/chinh-sach-thanh-toan"
+                  >
+                    Chính Sách Thanh Toán
+                  </Link>
+                </div>
+              </Col>
+              <Col className="ml-0 md:ml-[250px]">
+                <div className="flex flex-col space-y-1 md:space-y-4 font-mono">
+                  <span className="font-bold text-[18px] ">KẾT NỐI</span>
+                  <Flex wrap gap="small">
+                    <a
+                      href="https://www.facebook.com/tanthanh.bui.94617/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button
+                        shape="circle"
+                        style={{
+                          backgroundImage: `url(${facebooklogo})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                        }}
+                      />
+                    </a>
+                  </Flex>
+                  <Flex wrap gap="small">
+                    <a
+                      href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button
+                        shape="circle"
+                        style={{
+                          backgroundImage: `url(${animateLogo})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                        }}
+                      />
+                    </a>
+                  </Flex>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Footer>
+        <Footer
           style={{
-            padding: 24,
-            minHeight: 380,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
+            
+            textAlign: "center",
+            backgroundColor: "#36174d",
+            color: "white",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: 1,
+            width: "100%",
           }}
         >
-          {children}
-        </div>
-      </Content>
-      <Footer>
-        <div className="ml-12 mr-10">
-          <Row gutter={[48, 16]}>
-            <Col m>
-              <div className="flex flex-col space-y-6 font-mono">
-                <span className="font-bold text-[18px]">GIASUONLINE.VN</span>
-                <span className="font-normal text-[14px]">
-                  Địa chỉ: Lưu Hữu Phước, Đông Hoà, Dĩ An, Bình Dương
-                </span>
-                <span className="font-normal text-[14px]">
-                  Điện thoại: 0989897338
-                </span>
-                <span className="font-normal text-[14px]">
-                  Email: contact@giasuonline.vn
-                </span>
-              </div>
-            </Col>
-            <Col className="ml-[200px]">
-              <div className="flex flex-col space-y-6 font-mono ">
-                <span className="font-bold text-[18px]">LIÊN KẾT</span>
-                <div className="flex flex-row divide-x-2 divide-solid space-x-2 text-[#ff4778]">
-                  <Link className="font-normal text-[14px]" to="/gioi-thieu">
-                    Giới Thiệu
-                  </Link>
-                  <Link className="font-normal pl-2 text-[14px]" to="/lien-he">
-                    Liên Hệ
-                  </Link>
-                </div>
-                <div className="flex flex-row divide-x-2 divide-solid space-x-2 text-[#ff4778]">
-                  <Link className="font-normal text-[14px]" to="/tai-lieu">
-                    Tài Liệu
-                  </Link>
-                  <Link
-                    className="font-normal pl-2 text-[14px]"
-                    to="/kien-thuc"
-                  >
-                    Kiến Thức
-                  </Link>
-                  <Link
-                    className="font-normal pl-2 text-[14px]"
-                    to="/kinh-nghiem"
-                  >
-                    Kinh Nghiệm
-                  </Link>
-                </div>
-                <Link className="text-[#ff4778] text-[14px]" to="/login">
-                  Tài Khoản
-                </Link>
-              </div>
-            </Col>
-            <Col className="ml-[150px]">
-              <div className="flex flex-col space-y-6">
-                <span className="font-bold text-[18px]">CHÍNH SÁCH</span>
-                <Link
-                  className="text-[#ff4778] text-[14px]"
-                  to="/chinh-sach-nguoi-dung"
-                >
-                  Chính Sách Người Dùng
-                </Link>
-                <Link
-                  className="text-[#ff4778] text-[14px]"
-                  to="/dieu-khoan-su-dung"
-                >
-                  Điều Khoản Sử Dụng
-                </Link>
-                <Link
-                  className="text-[#ff4778] text-[14px]"
-                  to="/chinh-sach-thanh-toan"
-                >
-                  Chính Sách Thanh Toán
-                </Link>
-              </div>
-            </Col>
-            <Col className="ml-[250px]">
-              <div className="flex flex-col space-y-4">
-                <span className="font-bold text-[18px]">KẾT NỐI</span>
-                <Flex wrap gap="small">
-                  <a
-                    href="https://www.facebook.com/tanthanh.bui.94617/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button
-                      shape="circle"
-                      style={{
-                        backgroundImage: `url(${facebooklogo})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                      }}
-                    />
-                  </a>
-                </Flex>
-                <Flex wrap gap="small">
-                  <a
-                    href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button
-                      shape="circle"
-                      style={{
-                        backgroundImage: `url(${animateLogo})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                      }}
-                    />
-                  </a>
-                </Flex>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Footer>
-      <Footer
-        style={{
-          textAlign: "center",
-          backgroundColor: "#36174d",
-          color: "white",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: 1,
-          width: "100%",
-        }}
-      >
-        Gia sư online ©{new Date().getFullYear()} Công ty TNHH Nhóm 4 thành viên
-      </Footer>
+          Gia sư online ©{new Date().getFullYear()} Công ty TNHH Nhóm 4 thành
+          viên
+        </Footer>
+      </div>
     </Layout>
   );
 };
